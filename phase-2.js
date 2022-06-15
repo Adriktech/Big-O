d16350e38e7b8db0701f132d412cd61c4e8719f9
jack <jzhang319@gmail.com>
2022-06-14 20:27:19 -0400

    finished first half

--- phase-2.js
+++ phase-2.js
@@ -4,8 +4,11 @@ const [addNums, addManyNums] = require("./phase-1");
 function addNums10(increment) {
   // Fill this in
   let answer = [];
-  for (let i = 1; i <= 10; i += increment) {
-    answer.push(addNums(i));
+  let nextInc = increment;
+  for (let i = 1; i <= 10; i++) {
+    const addNumsRes = addNums(nextInc);
+    answer.push(addNumsRes);
+    nextInc += increment;
   }
   //console.log(answer + " <------ this is console.log");
   return answer;
@@ -15,12 +18,15 @@ function addNums10(increment) {
 function addManyNums10(increment) {
   // Fill this in
   let answer = [];
-  for (let i = 1; i <= 10; i += increment) {
-    
-    answer.push(addNums(i));
+  let nextInc = increment;
+  for (let i = 1; i <= 10; i++) {
+    const addNumsRes = addNums(nextInc);
+    answer.push(addNumsRes);
+    nextInc += increment;
   }
   //console.log(answer + " <------ this is console.log");
   return answer;
+
 }
 
 module.exports = [addNums10, addManyNums10];
