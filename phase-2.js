944b4631ac092dfb94d4993a5c2e8288ff26f0ed
jack <jzhang319@gmail.com>
2022-06-14 20:46:40 -0400

    finished phase 2

--- phase-2.js
+++ phase-2.js
@@ -18,15 +18,13 @@ function addNums10(increment) {
 function addManyNums10(increment) {
   // Fill this in
   let answer = [];
-  let nextInc = increment;
-  for (let i = 1; i <= 10; i++) {
-    const addNumsRes = addNums(nextInc);
+  let maxInc = increment * 10;
+  for (let i = increment; i <= maxInc; i += increment) {
+    const addNumsRes = addManyNums(i);
     answer.push(addNumsRes);
-    nextInc += increment;
   }
-  //console.log(answer + " <------ this is console.log");
-  return answer;
 
+  return answer;
 }
 
 module.exports = [addNums10, addManyNums10];
